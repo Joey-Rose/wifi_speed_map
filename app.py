61ac1779465160970b3576555f41c60ab41bf86a
Joey <joeyrose1999@gmail.com>
2021-10-08 00:25:31 -0700

    add set and get endpoints, map updates, code to test scalability

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
+import redis
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/free_ookla')
-def free_ookla(variable):
-    return "hello world"
+@app.route('/get_json')
+def get_json():
+    reply = r.execute_command('JSON.GET', 'geojson')
 
-@app.route('/mb')
-def mb():
-    return render_template('mapbox.html')
+    resp = Response(
+            reply.decode("utf-8"),
+            status=200,
+            mimetype='application/json'
+        )
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+@app.route('/set_json', methods = ['POST'])
+def set_json():
+    r.execute_command('JSON.ARRAPPEND', 'geojson', '.features', request.data)
 
 @app.route('/mb2')
 def mb2():
     return render_template('other_mapbox.html')
+
+@app.route('/wipe_redis_data')
+def wipe_redis_data():
+    inital_data = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    
+    reply = r.execute_command('JSON.SET', 'geojson', '.', json.dumps(inital_data))
+
+    return reply
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
--- redis_test.py
+++ redis_test.py
@@ -1,7 +1,11 @@
 import redis
 import json
+import random
+import time
+import threading
+import requests
 
-r = redis.Redis(host='redis-13143.c289.us-west-1-2.ec2.cloud.redislabs.com', port=13143, db=0, password='v5pV7Uj9UPU0mEsTUCJz6tlg74SYAYch')
+
 
 data = {
     'foo': 'bar'
@@ -12,8 +16,9 @@ geojson = {
                 {
                     'type': 'Feature',
                     'properties': {
-                        'description':
-                            'Download: 200 Mbps, Upload: 180 Mbps'
+                        "download": 190,
+                        "upload": 890,
+                        "date": "5/3/20"
                     },
                     'geometry': {
                         'type': 'Point',
@@ -25,9 +30,10 @@ geojson = {
 
 new_user_input = {
                     'type': 'Feature',
-                    'properties': {
-                        'description':
-                            'Download: 2000 Mbps, Upload: 1800 Mbps'
+                    "properties": {
+                        "download": 100,
+                        "upload": 80,
+                        "date": "5/3/20"
                     },
                     'geometry': {
                         'type': 'Point',
@@ -36,8 +42,54 @@ new_user_input = {
                 }
 # print(r.execute_command('KEYS *'))
 # r.execute_command('JSON.SET', 'doc', '.', json.dumps(data))
-# r.execute_command('JSON.SET', 'geojson', '.', json.dumps(geojson))
+# r.execute_command('JSON.SET', 'json', '.', json.dumps(geojson))
 # r.execute_command('JSON.ARRAPPEND', 'geojson', '.features', json.dumps(new_user_input))
-reply = json.loads(r.execute_command('JSON.GET', 'geojson'))
+# reply = json.loads(r.execute_command('JSON.GET', 'geojson'))
 
-print(reply)
+# print(reply)
+
+def add_rand_test():
+    download = random.randrange(1, 1001)
+    upload = random.randrange(1, 1001)
+    date = "5/3/20"
+    # Latitudes range from -90 to 90, and longitudes range from -180 to 90? instead of 80
+    lat = random.randrange(-90, 89) + round(random.random(), 6)
+    long = random.randrange(-180, 89) + round(random.random(), 6)
+    
+    new_user_input = {
+                    'type': 'Feature',
+                    "properties": {
+                        "download": download,
+                        "upload": upload,
+                        "date": date
+                    },
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [long, lat]
+                    }
+                }
+    # write_succeeded = False
+    # while write_succeeded == False:
+    #     try:
+    #         r.ping()
+    #         r.execute_command('JSON.ARRAPPEND', 'geojson', '.features', json.dumps(new_user_input))
+    #         write_succeeded = True
+    #     except:
+    #         # print("trying again")
+    #         time.sleep(1)
+    #         pass
+    # r.execute_command('JSON.ARRAPPEND', 'geojson', '.features', json.dumps(new_user_input))
+    print(requests.post("http://127.0.0.1:5001/set_json", data=json.dumps(new_user_input)).reason)
+
+start = time.time()
+for i in range(35):
+    threading.Thread(target=add_rand_test, args=()).start()
+    print("round:", i)
+    # if i == 17:
+    #     print(r.execute_command('JSON.GET', 'geojson').decode("utf-8"))
+    # add_rand_test()
+end = time.time()
+print(end-start, "many seconds passed")
+# reply = r.execute_command('JSON.GET', 'geojson')
+# with open("dynamic_data.json", "w") as f:
+#     f.write(reply.decode("utf-8"))
