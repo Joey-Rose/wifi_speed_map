eb7b53f9b0a2c14723f77e7f1bbab4901438c915
Joey <joeyrose1999@gmail.com>
2021-11-30 00:38:54 -0800

    Add auth page, add small stars for clusters to avoid overlap
    tmp checkpoint - stars are working, looks ugly compared to old clusters
    
    
    tmp: allow org way to let text display by adding layer later
    
    
    tmp: feature-state experiment, filter fix for stars, front end w/ auth
    
    
    tmp: logic to set cookie, auth to get into map, etc. WIP

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, Response
 import redis
 import json
+import jwt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234' # TODO: change this 
+
+r = redis.Redis(host="redis-13143.c289.us-west-1-2.ec2.cloud.redislabs.com", port=13143, db=0, password="v5pV7Uj9UPU0mEsTUCJz6tlg74SYAYch")
 
 @app.route('/')
 def index():
@@ -20,9 +24,22 @@ def get_json():
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
-@app.route('/set_json', methods = ['POST'])
+@app.route('/set_json', methods = ['POST', 'OPTIONS'])
 def set_json():
+    resp = Response(status=200)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Headers'] = '*'
+    if request.method == 'OPTIONS':
+        return resp
+    
+    print("request is", request.data)
+    payload = json.loads(request.data)
+    print("payload is", payload)
     r.execute_command('JSON.ARRAPPEND', 'geojson', '.features', request.data)
+    token = jwt.encode({'last_location': str(payload['geometry']['coordinates'][0]) + "," + str(payload['geometry']['coordinates'][1])}, app.config['SECRET_KEY'])
+    resp.headers['Set-Cookie'] = 'last_location=' + token
+
+    return resp
 
 @app.route('/mb2')
 def mb2():
